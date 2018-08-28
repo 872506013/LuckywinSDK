804d40e698fb4605a4969a2609d2a48f04233bf7
longxiang <ax123456>
2018-08-28 15:18:14 +0800

    项目创建

---
+++ app/src/main/java/lucky/sdk/luckywin/pk/MainActivity.java
@@ -0,0 +1,13 @@
+package lucky.sdk.luckywin.pk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
