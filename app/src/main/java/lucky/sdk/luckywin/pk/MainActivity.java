f449b3ef7d60e9ff56834d6996564a5ec95de7a3
longxiang <ax123456>
2018-08-29 16:07:22 +0800

    arr打包

--- app/src/main/java/lucky/sdk/luckywin/pk/MainActivity.java
+++ app/src/main/java/lucky/sdk/luckywin/pk/MainActivity.java
@@ -2,6 +2,12 @@ package lucky.sdk.luckywin.pk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.math.BigDecimal;
+
+import lucky.sdk.library.balance.GetBalance;
+import lucky.sdk.library.balance.OnBalanceListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GetBalance.getLuckywinBalance(new OnBalanceListener() {
+            @Override
+            public void onSuccess(BigDecimal bigDecimal) {
+                Log.i("1231234",bigDecimal+"");
+            }
+
+            @Override
+            public void onErro(String msg) {
+                Log.i("1231234",msg+"");
+            }
+        });
     }
 }
